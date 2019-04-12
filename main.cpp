3a45cea4bbb145c2d0335d766705c35a0eaa595e
Ocianus <fredrik1998@hotmail.com>
2019-04-12 17:42:28 +0200

    løk

--- main.cpp
+++ main.cpp
@@ -38,14 +38,6 @@ public:
         cout << "Welcome to registration. Please enter the values of the book you wish"
                 "to add to the shelf " << endl;
 
-        if(fout.is_open()) {
-            fout.seekp(0, ios::end);
-            size_t size = fout.tellp();
-            if(size == 0) {
-                fout << "Title - ISBN - Author - Pages - Publish year" << endl;
-            }
-        }
-
         while (fout) {
             cout << "Enter 0 if you no longer wish to add more books,"
                     "if you wish to proceed adding new book press any key: " << endl;
@@ -57,7 +49,7 @@ public:
             getline(cin, title);
             cout << "Enter ISBN number: " << endl;
             getline(cin, isbnNumber);
-            cout << "Enter name of author: " << endl;
+            cout << "Enter name of author (first and last name, no middle name): " << endl;
             getline(cin, author);
             cout << "Enter the number of pages: " << endl;
             getline(cin, pages);
@@ -65,9 +57,6 @@ public:
             getline(cin, publishYear);
 
             fout << title << " " << isbnNumber << " " << author << " " << pages << " " << publishYear << endl;
-            //fout << "Date added to library: " << libraryID << endl;
-            //time_t tt; struct tm * ti; time(&tt); ti = localtime(&tt);
-            //librarian1.startDate = asctime(ti);
             cout << "Book successfully added!" << endl;
         }
         fout.close();
@@ -97,6 +86,7 @@ public:
         }
 
         if(fout.is_open()) {
+            cout << "Title - ISBN - Author - Pages - Publish date" << endl;
             cout << fout.rdbuf();
         }
     }
@@ -164,14 +154,6 @@ public:
         fout.open("librarian.txt", std::ios_base::app);
         cout << "Welcome to registration. Please fill in the information below: " << endl;
 
-        if(fout.is_open()) {
-            fout.seekp(0, ios::end);
-            size_t size = fout.tellp();
-            if(size == 0) {
-                fout << "First name - Last name - Address - Phonenumber - Employee ID - Workplace - Hire date"  << endl;
-            }
-        }
-
         while(fout) {
             cout << "Enter 0 if you no longer wish to add more members,"
                     "if you wish to proceed adding new member press any key: " << endl;
@@ -179,20 +161,20 @@ public:
             if(line == "0") {
                 break;
             }
-            cout << "Enter the librarians first name: " << endl;
+            cout << "Enter the librarian's first name: " << endl;
             getline(cin, firstName);
-            cout << "Enter the librarians last name: " << endl;
+            cout << "Enter the librarian's last name: " << endl;
             getline(cin, lastName);
-            cout << "Enter the librarians address: " << endl;
+            cout << "Enter the librarian's address: " << endl;
             getline(cin, address);
-            cout << "Enter the librarians phone number: " << endl;
+            cout << "Enter the librarian's phone number: " << endl;
             getline(cin, phoneNumber);
-            cout << "Enter the library the employee will be working at " << endl;
+            cout << "Enter the name of the city the library the employee will be working at is located " << endl;
             getline(cin, libraryID);
 
             time_t tt; struct tm * ti; time(&tt); ti = localtime(&tt);
             librarian1.startDate = asctime(ti);
-            fout << firstName << lastName <<  address <<  phoneNumber << employeeID << libraryID << librarian1.startDate << endl;
+            fout << firstName << " " << lastName << " " <<  address << " " << phoneNumber << " " << employeeID << " " << libraryID << " " << librarian1.startDate;
             employeeID++;
             cout << "Librarian successfully added!" << endl;
         }
@@ -224,12 +206,13 @@ public:
         }
 
         if(fout.is_open()) {
+            cout << "First and last name - Address - Phone number - Employee ID - Date hired" << endl;
             cout << fout.rdbuf();
         }
     }
 
     void removeLibrarian() {
-        string name, fname, lname, phone, address, date, a6, a7, a8, a9, a10, a11;
+        string name, fname, lname, phone, address1, address2, date1, date2, date3, date4, date5, date6, date7;
         int x = 0;
 
         ifstream librarian("librarian.txt");
@@ -238,10 +221,10 @@ public:
         cout << "Enter name of the librarian you want to erase from database >" << endl;
         cin >> fname;
 
-        while(librarian >> name >> lname >> phone >> address >> date >> a6 >> a7 >> a8 >> a9 >> a10 >> a11) {
+        while(librarian >> name >> lname >> phone >> address1 >> address2 >> date1 >> date2 >> date3 >> date4 >> date5 >> date6 >> date7) {
             if(fname!=name) {
-                temp << name << ' ' << lname << ' ' << phone << ' ' << address << ' ' << date
-                     << ' ' << a6 << ' ' << a7 << ' ' << a8 << ' ' << a9 << ' ' << a10 << ' ' << a11 <<endl;
+                temp << name << ' ' << lname << ' ' << phone << ' ' << address1 << ' ' << address2
+                     << ' ' << date1 << ' ' << date2 << ' ' << date3 << ' ' << date4 << ' ' << date5 << ' ' << date6 << ' ' << date7 <<endl;
             } if(fname==name) {
                 x = 1;
             }
@@ -285,14 +268,6 @@ public:
         fout.open("member.txt", std::ios_base::app);
         cout << "Welcome to member registration. Please fill in the information below: " << endl;
 
-        if(fout.is_open()) {
-            fout.seekp(0, ios::end);
-            size_t size = fout.tellp();
-            if(size == 0) {
-                fout << "First name - Last name - Address - Phonenumber - Member ID - Registration date" << endl;
-            }
-        }
-
         while(fout) {
             cout << "Enter 0 if you no longer wish to add more members,"
                     "if you wish to proceed adding a new member press any key: " << endl;
@@ -300,18 +275,18 @@ public:
             if(line == "0") {
                 break;
             }
-            cout << "Enter the memebers first name: " << endl;
+            cout << "Enter the members first name: " << endl;
             getline(cin, firstName);
-            cout << "Enter the memembers last name: " << endl;
+            cout << "Enter the members last name: " << endl;
             getline(cin, lastName);
-            cout << "Enter the memembers address: " << endl;
+            cout << "Enter the members address: " << endl;
             getline(cin, address);
-            cout << "Enter the memembers phone number: " << endl;
+            cout << "Enter the members phone number: " << endl;
             getline(cin, phoneNumber);
 
             time_t tt; struct tm * ti; time(&tt); ti = localtime(&tt);
             member1.registrationDate = asctime(ti);
-            fout << firstName << lastName <<  address << phoneNumber <<  id << member1.registrationDate << endl;
+            fout << firstName << " " <<  lastName << " " << address << " " << phoneNumber << " " << id << " " << member1.registrationDate;
             id++;
             cout << "Member successfully added!" << endl;
         }
@@ -341,12 +316,13 @@ public:
         }
 
         if(fout.is_open()) {
+            cout << "First and last name - Address - Phone number - Member ID - Date registered" << endl;
             cout << fout.rdbuf();
         }
     }
 
     void removeMember() {
-        string name, fname, lname, phone, address, date, a6, a7, a8, a9, a10, a11;
+        string name, fname, lname, phone, address1, address2, date1, date2, date3, date4, date5, date6;
         int x = 0;
 
         ifstream member("member.txt");
@@ -355,10 +331,10 @@ public:
         cout << "Enter name of the member you want to erase from database >" << endl;
         cin >> fname;
 
-        while(member >> name >> lname >> phone >> address >> date >> a6 >> a7 >> a8 >> a9 >> a10 >> a11) {
+        while(member >> name >> lname >> phone >> address1 >> address2 >> date1 >> date2 >> date3 >> date4 >> date5 >> date6) {
             if(fname!=name) {
-                temp << name << ' - ' << lname << ' - ' << phone << ' - ' << address << ' - ' << date
-                     << ' - ' << a6 << ' - ' << a7 << ' - ' << a8 << ' - ' << a9 << ' - ' << a10 << ' - ' << a11 <<endl;
+                temp << name << ' ' << lname << ' ' << phone << ' ' << address1 << ' ' << address2
+                     << ' ' << date1 << ' ' << date2 << ' ' << date3 << ' ' << date4 << ' ' << date5 << ' ' << date6 <<endl;
             } if(fname==name) {
                 x = 1;
             }
@@ -368,8 +344,8 @@ public:
         member.seekg(0, ios::beg);
         member.close();
         temp.close();
-        remove("renter.txt");
-        rename("temp.txt", "renter.txt");
+        remove("member.txt");
+        rename("temp.txt", "member.txt");
         if(x==0) {
             cout << "There is no member with the name you entered." << endl;
         } else {
@@ -404,14 +380,6 @@ public:
 
         fout.open("renter.txt", std::ios_base::app);
         cout << "Welcome to registration. Please fill in the information below: " << endl;
-
-        if(fout.is_open()) {
-            fout.seekp(0, ios::end);
-            size_t size = fout.tellp();
-            if(size == 0) {
-                fout << "First name - Last name - Address - Phonenumber - Depositum - Rent time" << endl;
-            }
-        }
 
         while(fout) {
             cout << "Enter 0 if you no longer wish to add more renters,"
@@ -468,12 +436,13 @@ public:
         }
 
         if(fout.is_open()) {
+            cout << "First and last name - Address - Phone number - Date rented" << endl;
             cout << fout.rdbuf();
         }
     }
 
     void removeRenter() {
-        string name, fname, lname, phone, address, date, a6, a7, a8, a9, a10, a11;
+        string name, fname, lname, phone, address1, address2, date1, date2, date3, date4, date5, date6;
         int x = 0;
 
         ifstream renter("renter.txt");
@@ -482,10 +451,9 @@ public:
         cout << "Enter name of the renter you want to erase from database >" << endl;
         cin >> fname;
 
-        while(renter >> name >> lname >> phone >> address >> date >> a6 >> a7 >> a8 >> a9 >> a10 >> a11) {
+        while(renter >> name >> lname >> phone >> address1 >> address2 >> date1 >> date2 >> date3 >> date4 >> date5 >> date6) {
             if(fname!=name) {
-                temp << name << ' ' << lname << ' ' << phone << ' ' << address << ' ' << date
-                << ' ' << a6 << ' ' << a7 << ' ' << a8 << ' ' << a9 << ' ' << a10 << ' ' << a11 <<endl;
+                temp << name << ' ' << lname << ' ' << phone << ' ' << address1 << ' ' << address2 << ' ' << date1 << ' ' << date2 << ' ' << date3 << ' ' << date4 << ' ' << date5 << ' ' << date6 <<endl;
             } if(fname==name) {
                 x = 1;
             }
@@ -511,15 +479,19 @@ int main() {
     //renter1.getRenterInfo();
     //renter1.removeRenter();
 
-    //Member member1;
+    Member member1;
     //member1.makeMember();
+    //member1.removeMember();
+    member1.listMembers();
 
-    //Librarian librarian;
+    Librarian librarian;
     //librarian.makeLibrarian();
     //librarian.listLibrarians();
+    //librarian.removeLibrarian();
 
     Book book;
     //book.addBook();
-    book.removeBook();
+    //book.removeBook();
+    //book.listBooks();
     return 0;
 }
